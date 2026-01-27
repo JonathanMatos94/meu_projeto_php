e0dd65c05ab8bcd40db8429725e391486515f61f
Jonathan Matos <carvalhomatos.jonathan@gmail.com>
2026-01-27 19:02:26 -0300

    mais conteudo

---
+++ estrutura_repeticao/break.php
@@ -0,0 +1,16 @@
+<?php
+
+$count = 0;
+$objetivo = 130;
+
+while(true){
+
+    echo "$count // ";
+    $count++;
+
+    if($count == $objetivo){
+        echo "$count chegou no objetivo";
+        break;
+    }
+
+}
---
+++ estrutura_repeticao/while.php
@@ -0,0 +1,10 @@
+<?php
+
+$count = 0;
+
+while($count < 100){
+    echo "$count // ";
+    $count++;
+}
+
+echo "Acabou";
